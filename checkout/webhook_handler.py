24253ff15a470d7e11b2c07258630ec9051bb07e
Wasim Alrawas <wassimrawwas@gmail.com>
2024-07-21 04:51:19 +0000

    fix stripe keys in settings

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -26,11 +26,13 @@ class StripeWH_Handler:
         pid = intent.id
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
-
-        billing_details = intent.charges.data[0].billing_details
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
+        billing_details = stripe_charge.billing_details
         shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
-
+        grand_total = round(stripe_charge.amount / 100, 2)
+        
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
             if value == "":
