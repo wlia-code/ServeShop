f773d3b2f378183ba4b377da26ecf1589849d5af
Wasim Alrawas <wassimrawwas@gmail.com>
2024-07-23 13:44:06 +0000

    Add custom 404 page

--- serveshop/settings.py
+++ serveshop/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
 import dj_database_url
 
 if os.path.isfile('env.py'):
-    import env
+    import env  # noqa: F401
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,17 +30,21 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/','127.0.0.1:8000','localhost',
-                '8000-wlia-code-serveshop-qkykmzngrj.us1.codeanyapp.com',
-                '8000-wliacode-serveshop-1zv5a2xpu0c.ws.codeinstitute-ide.net',
-                'https://gn-shop-e18f4dd2529c.herokuapp.com/',
-                'gn-shop-e18f4dd2529c.herokuapp.com',]
+ALLOWED_HOSTS = [
+    'http://127.0.0.1:8000/',
+    '127.0.0.1:8000',
+    'localhost',
+    '8000-wlia-code-serveshop-qkykmzngrj.us1.codeanyapp.com',
+    '8000-wliacode-serveshop-1zv5a2xpu0c.ws.codeinstitute-ide.net',
+    'https://gn-shop-e18f4dd2529c.herokuapp.com/',
+    'gn-shop-e18f4dd2529c.herokuapp.com',
+]
 
 CSRF_TRUSTED_ORIGINS = [
-
                         "https://*.codeanyapp.com",
                         "https://*.herokuapp.com",
-                        "https://*.gitpod.io",'https://8000-wliacode-serveshop-1zv5a2xpu0c.ws.codeinstitute-ide.net',]
+                        "https://*.gitpod.io",
+                        ]
 
 # Application definition
 
@@ -143,16 +147,28 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
     },
 ]
 
