a974ea8d9205b89400658ac662dff653b1f9e72b
Wasim Alrawas <wassimrawwas@gmail.com>
2024-07-23 02:59:19 +0000

    fix local static root

--- serveshop/settings.py
+++ serveshop/settings.py
@@ -199,6 +199,11 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
+else:
+    # Local static files settings
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
